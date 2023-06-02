111c7e0ce63b55cfb32c406c09a12ddf41f4cbc2
Trung <trung@thebigredhouse.com.au>
2023-06-02 17:39:28 +0700

    SDK v1.2

--- src/SDK/constants/index.ts
+++ src/SDK/constants/index.ts
@@ -1,6 +1,14 @@
+import { FeatureFlagType } from '../../types/KindeSDK';
+
 export const AdditionalParametersAllow = {
     audience: 'string',
     is_create_org: 'boolean',
     org_code: 'string',
     org_name: 'string'
+};
+
+export const FLAG_TYPE: Record<FeatureFlagType, string> = {
+    s: 'string',
+    i: 'number', // integer
+    b: 'boolean'
 };
